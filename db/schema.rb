aeb478d8cd3092b172a78e0fb79fa8e1a1d0c147
Daniel Kehoe <daniel@danielkehoe.com>
2015-01-01 18:08:39 +0200

    Rails 4.2

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141028001605) do
+ActiveRecord::Schema.define(version: 20150101155005) do
 
-  create_table "visitors", force: true do |t|
+  create_table "visitors", force: :cascade do |t|
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
