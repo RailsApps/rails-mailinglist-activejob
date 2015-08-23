c49c3dcdab92218ea8da27d430d6a06b92d66052
Daniel Kehoe <daniel@danielkehoe.com>
2015-08-23 17:16:48 +0800

    update for Gibbon 2.0 fixes #3

--- app/jobs/mailing_list_signup_job.rb
+++ app/jobs/mailing_list_signup_job.rb
@@ -1,8 +1,19 @@
 class MailingListSignupJob < ActiveJob::Base
 
-  def perform(visitor)
-    logger.info "signing up #{visitor.email}"
-    visitor.subscribe
+  def perform(user)
+    logger.info "signing up #{user.email}"
+    subscribe(user)
+  end
+
+  def subscribe(user)
+    mailchimp = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key)
+    list_id = Rails.application.secrets.mailchimp_list_id
+    result = mailchimp.lists(list_id).members.create(
+      body: {
+        email_address: user.email,
+        status: 'subscribed'
+    })
+    Rails.logger.info("Subscribed #{user.email} to MailChimp") if result
   end
 
 end
--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,22 +1,10 @@
 class Visitor < ActiveRecord::Base
   validates_presence_of :email
   validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
-  after_create :sign_up_for_mailing_list
-
-  def sign_up_for_mailing_list
-    MailingListSignupJob.perform_later(self)
-  end
+  after_create :subscribe
 
   def subscribe
-    mailchimp = Gibbon::API.new(Rails.application.secrets.mailchimp_api_key)
-    result = mailchimp.lists.subscribe({
-      :id => Rails.application.secrets.mailchimp_list_id,
-      :email => {:email => self.email},
-      :double_optin => false,
-      :update_existing => true,
-      :send_welcome => true
-    })
-    Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
+    MailingListSignupJob.perform_later(self)
   end
 
 end
