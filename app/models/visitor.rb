9a94af1cfad7fd806c796cbc92cc6218ccd2cf65
Daniel Kehoe <daniel@danielkehoe.com>
2016-10-12 15:04:01 +0800

    update to Rails 5.0

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,4 +1,4 @@
-class Visitor < ActiveRecord::Base
+class Visitor < ApplicationRecord
   validates_presence_of :email
   validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
   after_create :subscribe
